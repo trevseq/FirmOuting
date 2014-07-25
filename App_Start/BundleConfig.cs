19bbd8938c179cda947d4287ce3d6530618b9978
trvseq2 <trvseq9910@gmail.com>
2014-07-25 10:17:38 -0400

    Added files

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,65 @@
+﻿using System.Web.Optimization;
+
+namespace FirmOuting
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Enable CDN support
+            // bundles.UseCdn = true;
+
+            // Enable Bundling and Minification
+             BundleTable.EnableOptimizations = false;
+
+
+
+            #region Script Bundling
+            //Add link to jquery on the CDN
+            //var cdnJqueryPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.0.min.js";
+            //var cdnJqueryUIPath = "http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/jquery-ui.min.js";
+
+            // Add link to jquery on the CDN
+            //bundles.Add(new ScriptBundle("~/bundles/jquery", cdnJqueryPath));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryUI", cdnJqueryUIPath));
+
+            // Login Layout bundle
+            bundles.Add(new ScriptBundle("~/bundles/loginScripts").Include(
+                        "~/scripts/jqbrowser-uncompressed.js",
+                         "~/scripts/explorer-extra.js",
+                         "~/scripts/default.js"));
+
+            // Client Styl
+            bundles.Add(new ScriptBundle("~/bundles/clientScripts").Include(
+               "~/Scripts/Plugins/jquery-dialogextend-master/build/jquery.dialogextend.min.js",
+                "~/Scripts/Plugins/posabsolute-jQuery-Validation-Engine-499f567/js/languages/jquery.validationEngine-en.js",
+                "~/Scripts/Plugins/posabsolute-jQuery-Validation-Engine-499f567/js/jquery.validationEngine.js",
+
+                         "~/Scripts/Plugins/jquery-cookie/jquery-cookie.js",
+                         "~/scripts/jqbrowser-uncompressed.js",
+                         "~/scripts/pqgrid.min.js",
+                         "~/scripts/explorer-custom.js"));
+           
+            #endregion
+            #region Style Bundling
+
+            // Login Style
+            bundles.Add(new StyleBundle("~/css/loginStyles")
+                               .Include("~/Content/style-explorer.css",
+                                "~/Content/style-custom.css",
+                                "~/Content/themes/redmond/jquery-ui-1.10.3.custom.min.css"));
+
+            // Client Style
+            bundles.Add(new StyleBundle("~/css/clientStyles")
+                               .Include("~/Content/style-explorer.css",
+                                "~/Content/style-custom.css",
+                                "~/Content/pqgrid.min.css",
+                                "~/Scripts/Plugins/posabsolute-jQuery-Validation-Engine-499f567/css/validationEngine.jquery.css",
+                                "~/Content/themes/redmond/jquery-ui-1.10.3.custom.min.css"));
+            #endregion
+
+
+        }
+    }
+}
---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FirmOuting
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapHttpRoute(
+                           name: "DefaultApi",
+                           routeTemplate: "api/{controller}/{id}",
+                           defaults: new { id = RouteParameter.Optional }
+                       );
+
+            routes.MapRoute(
+                name: "Secure",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Secure", action = "Default", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Home",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Default", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
