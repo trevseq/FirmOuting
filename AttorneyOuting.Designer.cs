22f76722cac81142557fd25c2f5c2315dd3509f7
trevseq <trvseq9910@gmail.com>
2014-08-26 11:58:59 -0400

    attempted connection string fix

--- AttorneyOuting.Designer.cs
+++ AttorneyOuting.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Projects\FirmOuting\FirmOuting\AttorneyOuting.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Projects\Repos\FirmOuting\FirmOuting\AttorneyOuting.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
